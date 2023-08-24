0a4adaf8ad2ac988a03140f0eaae62cc028fb497
siddhithawal <siddhithawal@localhost>
2023-08-24 17:45:19 +0530

    test change

--- src/main/java/com/insurance/vehicleInsurance/controller/PaymentController.java
+++ src/main/java/com/insurance/vehicleInsurance/controller/PaymentController.java
@@ -2,7 +2,7 @@ package com.insurance.vehicleInsurance.controller;
 
 public class PaymentController {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
 	}
 
